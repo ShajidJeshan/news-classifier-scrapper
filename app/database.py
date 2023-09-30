2cf70cfb1c4951495e891b637ab3c0d61c6252ac
ShajidJeshan <shajidjeshan@gmail.com>
2023-09-30 17:45:10 +0600

    added .env

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:jeshan46@localhost:5432/history_db"
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('PASSWORD')}@{os.getenv('HOST')}:{os.getenv('PORT')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
--- app/main.py
+++ app/main.py
@@ -4,15 +4,19 @@ from . import models
 from .routers import history, scrape
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
+import uvicorn
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=os.getenv('ALLOWED_ORIGIN'),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,3 +26,6 @@ add_pagination(app)
 
 app.include_router(scrape.router)
 app.include_router(history.router)
+
+if __name__ == '__main__':
+    uvicorn.run('app.main:app', reload=True)
