33afc2574e578c2a12561bf70485ec61ae4a33de
ShajidJeshan <shajidjeshan@gmail.com>
2023-10-01 05:10:33 +0600

    db url added

--- app/database.py
+++ app/database.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
+# import os
 from dotenv import load_dotenv
 load_dotenv()
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('PORT')}/{os.getenv('POSTGRES_DATABASE')}"
-
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('PORT')}/{os.getenv('POSTGRES_DATABASE')}"
+SQLALCHEMY_DATABASE_URL = "postgres://postgresuser:r0wU9yJlm7sdPkyJSlEz2Ma6fEPyo37A@dpg-ckc98duct0pc73alrong-a/history_db_giup"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
