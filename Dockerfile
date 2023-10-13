61090aed185939229009ce2d889d8f231e763135
ShajidJeshan <shajidjeshan@gmail.com>
2023-10-13 20:28:14 +0600

    added docker-compose & fixed minor issues

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-# FROM  python:3.10.13-alpine3.18
-# WORKDIR /api
-# COPY . /api
-# RUN pip install -r requirements.txt
-# EXPOSE 8000
-# CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+#FROM  python:3.10.13-alpine3.18
+FROM python:3.10.12-slim-bullseye
+RUN echo "nameserver 8.8.8.8" > /etc/resolv.conf
+WORKDIR /api
+COPY . /api
+RUN pip3 install --no-cache-dir --upgrade -r requirements.txt
+EXPOSE 8000
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- alembic/env.py
+++ alembic/env.py
@@ -11,8 +11,8 @@ load_dotenv()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-# config.set_main_option(f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('PORT')}/{os.getenv('POSTGRES_DATABASE')}")
-config.set_main_option("postgresql://postgresuser:r0wU9yJlm7sdPkyJSlEz2Ma6fEPyo37A@dpg-ckc98duct0pc73alrong-a/history_db_giup")
+config.set_main_option(f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('PORT')}/{os.getenv('POSTGRES_DATABASE')}")
+#config.set_main_option("postgresql://postgresuser:r0wU9yJlm7sdPkyJSlEz2Ma6fEPyo37A@dpg-ckc98duct0pc73alrong-a/history_db_giup")
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
